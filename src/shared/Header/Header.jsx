11921808eeb1bf5f44530ab0830becbc04b9d302
devtabibur <tobiburrohman2@gmail.com>
2022-08-08 01:57:39 +0600

    avata and name is done on header

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -32,13 +32,63 @@ const Header = ({ children }) => {
             <div className="flex-1 text-xl font-medium text-white">
               E-Learning
             </div>
-            {/* profile */}
-            <div className="dropdown dropdown-end">
-              <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img src="https://placeimg.com/80/80/people" />
+            {/* notifications */}
+            {/* <div className="dropdown dropdown-end">
+              <label tabIndex="0" className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                  <span className="badge badge-sm indicator-item">8</span>
                 </div>
               </label>
+              <div
+                tabIndex="0"
+                className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+              >
+                <div className="card-body">
+                  <span className="font-bold text-lg">8 Items</span>
+                  <span className="text-info">Subtotal: $999</span>
+                  <div className="card-actions">
+                    <button className="btn btn-primary btn-block">View cart</button>
+                  </div>
+                </div>
+              </div>
+            </div> */}
+
+            {/* profile / admin */}
+
+            <div className="dropdown dropdown-end">
+              <div className="flex">
+                <label
+                  tabIndex="0"
+                  className="btn btn-ghost btn-circle w-20 mr-5"
+                >
+                  <div className="indicator mt-0 pt-0">
+                    <h2>John Doe</h2>
+                    <span className="badge badge-sm indicator-item -mt-2 lowercase">
+                      Admin
+                    </span>
+                  </div>
+                </label>
+                <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img src="https://placeimg.com/80/80/people" />
+                  </div>
+                </label>
+              </div>
+
               <ul
                 tabIndex="0"
                 className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-accent rounded-box w-52"
@@ -53,7 +103,7 @@ const Header = ({ children }) => {
                   <NavLink to="/settings">Settings</NavLink>
                 </li>
                 <li>
-                  <NavLink to='/logout'>Logout</NavLink>
+                  <NavLink to="/logout">Logout</NavLink>
                 </li>
               </ul>
             </div>
